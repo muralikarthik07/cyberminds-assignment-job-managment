d8ce06ce86cbbbfb5c99aabe11363a45d34c2f1a
karthik <muralikarthik07@gmail.com>
2025-07-24 23:21:05 +0530

    MYSQL CHANGES

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // MySQL Connection
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306 
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306, 
 });
 
 // Connect to MySQL
